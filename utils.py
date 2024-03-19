35b35db44a8b67b9cbaeb53272b5df12a397cd84
Fabian Gröger <fabian.groeger@bluewin.ch>
2024-03-19 13:05:04 +0100

    fixed problem with colab and formatted notebooks

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import os
+import tarfile
 from pathlib import Path
-import pandas as pd
-from torchvision import datasets
 from typing import Union
+
+import pandas as pd
+import requests
+from torchvision import datasets
 
 OXFORD_PETS_URL = "https://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz"
 
@@ -16,14 +19,24 @@ def get_oxford_pets3t(
     return_dataframe: bool = False,
     **kwargs,
 ):
-    if not Path(root_path).exists():
-        os.system(f"/bin/bash -c \"wget -nc '{OXFORD_PETS_URL}'\"")
-        os.system(f'/bin/bash -c "mkdir -p {root_path}"')
-        os.system(f'/bin/bash -c "tar -xf images.tar.gz -C {root_path}/"')
+    root_path = Path(root_path)
+    if not root_path.exists():
+        # download the dataset
+        response = requests.get(OXFORD_PETS_URL, stream=True)
+        tar_path = root_path / "images.tar.gz"
+        with open(tar_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                f.write(chunk)
+        # create the directory and extract the file
+        root_path.mkdir(parents=True, exist_ok=True)
+        with tarfile.open(tar_path) as tar:
+            tar.extractall(path=root_path)
+        # remove the tar file after extraction
+        os.remove(tar_path)
     else:
         print(f"Oxford PetIIIT already downloaded to `{root_path}`.")
 
-    dataset = datasets.ImageFolder(root=root_path, **kwargs)
+    dataset = datasets.ImageFolder(root=str(root_path), **kwargs)
     classes = list(
         set([class_name_from_file(samples[0]) for samples in dataset.samples])
     )
@@ -40,8 +53,7 @@ def get_oxford_pets3t(
     dataset.samples = samples
 
     if return_dataframe:
-        df = pd.DataFrame(dataset.samples)
-        df.columns = ["img_path", "label"]
+        df = pd.DataFrame(samples, columns=["img_path", "label"])
         df["label_name"] = df["label"].apply(lambda x: dataset.classes[x])
         df["img_path"] = df["img_path"].astype(str)
         return dataset, df
